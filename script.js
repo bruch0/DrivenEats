2ea368255eae48d4519ffbd4c459bc939c63048b
bruch0 <lucas.bruch0@gmail.com>
2021-07-21 14:39:11 -0300

    changed the divs for buttons for better semantic and accessibility

---
+++ script.js
@@ -0,0 +1,9 @@
+function selectMainDish1 () {
+    document.getElementById("main-1").style.border = "5px solid #32B72F";
+    document.getElementById("main-2").style.border = "5px solid transparent";
+}
+
+function selectMainDish2 () {
+    document.getElementById("main-2").style.border = "5px solid #32B72F";
+    document.getElementById("main-1").style.border = "5px solid transparent";
+}
